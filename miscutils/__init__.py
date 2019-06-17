bbbc2eb76e4143ecc8bd6862c1d28bceba44b8c1
matthewgdv <matthewgdv@gmail.com>
2019-06-17 15:54:35 +0100

    Fix resources issue

--- miscutils/__init__.py
+++ miscutils/__init__.py
@@ -7,13 +7,14 @@ __all__ = [
     "Log", "PrintLog"
 ]
 
-from .misc import is_running_in_ipython, Beep, Version, Counter, EnVars, WhoCalledMe, NameSpace
-from .context import SysTrayApp, Timer, Supressor, PrintRedirector, NullContext
-from .serializer import Serializer, ByteSerializer, Secrets
-from .script import ScriptBase
-from .commandline import CommandLine
-from .log import Log, PrintLog
-
 from pathmagic import File
 
-res = File(__file__).dir.newdir("res")
+resources = File(__file__).dir.newdir("res")
+
+if True:
+    from .misc import is_running_in_ipython, Beep, Version, Counter, EnVars, WhoCalledMe, NameSpace
+    from .context import SysTrayApp, Timer, Supressor, PrintRedirector, NullContext
+    from .serializer import Serializer, ByteSerializer, Secrets
+    from .script import ScriptBase
+    from .commandline import CommandLine
+    from .log import Log, PrintLog
