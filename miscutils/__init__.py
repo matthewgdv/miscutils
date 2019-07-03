57d2ad131794478e0fb683b58b50cb9ee019b53d
matthewgdv <matthewgdv@gmail.com>
2019-07-03 10:05:26 +0100

    Refactor and move Singleton class into miscutils

--- miscutils/__init__.py
+++ miscutils/__init__.py
@@ -1,4 +1,5 @@
 __all__ = [
+    "Singleton",
     "is_running_in_ipython", "Beep", "Version", "Counter", "EnVars", "WhoCalledMe", "NameSpace",
     "SysTrayApp", "Beep", "Timer", "Supressor", "PrintRedirector", "NullContext",
     "Serializer", "ByteSerializer", "Secrets",
@@ -16,6 +17,7 @@ from pathmagic import File
 resources = File(__file__).dir.newdir("res")
 
 if True:
+    from .singleton import Singleton
     from .misc import is_running_in_ipython, Beep, Version, Counter, EnVars, WhoCalledMe
     from .context import SysTrayApp, Timer, Supressor, PrintRedirector, NullContext
     from .serializer import Serializer, ByteSerializer, Secrets
