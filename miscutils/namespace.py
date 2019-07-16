d8610e0882c9678336e1bd3d21f600a25978247b
matthewgdv <matthewgdv@gmail.com>
2019-07-16 16:02:43 +0100

    Refactor NameSpace to be simpler

--- miscutils/namespace.py
+++ miscutils/namespace.py
@@ -3,60 +3,38 @@ from __future__ import annotations
 from typing import Any, Dict
 
 from maybe import Maybe
-from subtypes import Str
 
 
 class NameSpace:
     def __init__(self, mappings: Dict[str, Any] = None) -> None:
-        self._namespace = {}
-        self._update_namespace(mappings)
+        self.__dict__.update(Maybe(mappings).else_({}))
 
     def __repr__(self) -> str:
         return f"{type(self).__name__}({', '.join([f'{attr}={repr(val)}' for attr, val in self.__dict__.items() if not attr.startswith('_')])})"
 
     def __getitem__(self, name: str) -> Any:
-        return self._namespace[name]
+        return getattr(self, name)
 
     def __setitem__(self, name: str, val: Any) -> None:
-        self._add_to_namespace(name, val)
+        setattr(self, name, val)
 
     def __delitem__(self, name: str) -> None:
-        self._remove_from_namespace(name)
-
-    def __setattr__(self, name: str, val: Any) -> None:
-        if name.startswith("_"):
-            super().__setattr__(name, val)
-        else:
-            self._add_to_namespace(name, val)
-
-    def __delattr__(self, name: str) -> None:
-        self._remove_from_namespace(name)
+        self.__delattr__(name)
 
     def __len__(self) -> int:
-        return len(self._namespace)
+        return len([item for item in self])
 
     def __iter__(self) -> NameSpace:
-        self.__iter = self._namespace.values()
+        self.__iter = iter({name: val for name, val in vars(self).items() if not name.startswith("_")}.items())
         return self
 
     def __next__(self) -> Any:
         return next(self.__iter)
 
     def __contains__(self, other: Any) -> bool:
-        return other in set(self._namespace.keys())
-
-    def _add_to_namespace(self, name: Any, val: Any) -> None:
-        super().__setattr__(Str(name).identifier(), val)
-        self._namespace[name] = val
-
-    def _update_namespace(self, mappings: Dict[str, Any]) -> None:
-        for key, val in Maybe(mappings).else_({}).items():
-            self[key] = val
-
-    def _remove_from_namespace(self, name: str) -> None:
-        super().__delattr__(name)
-        self._namespace.pop(name)
+        return other in set(vars(self).keys())
 
     def _clear_namespace(self) -> None:
-        for name in list(self._namespace):
+        to_delete = [key for key in vars(self).keys() if not key.startswith("_")]
+        for name in to_delete:
             del self[name]
