e9ad8b05d79599a53222171dd1fe80ded7a16351
matthewgdv <matthewgdv@gmail.com>
2019-07-26 15:15:35 +0100

    Further refactor to serializer

--- miscutils/serializer.py
+++ miscutils/serializer.py
@@ -90,24 +90,13 @@ class Serializer:
 
 class UnpickleableItemHelper:
     def __init__(self, item: Any) -> None:
-        self.item, self.seen, self.seen_while_filling = item, {id(item): None}, set()
+        self.item, self.seen = item, {}
 
     def serializable_copy(self) -> Any:
-        try:
-            initial_copy = copy.copy(self.item)
-        except TypeError:
-            return LostObject(self.item)
-        else:
-            pending = self.recursively_strip_invalid(initial_copy)
-            self.seen[id(self.item)] = pending
-            self.fill_in_future_objects(pending)
-            return pending
+        return self.recursively_strip_invalid(self.item)
 
     def recursively_strip_invalid(self, obj) -> Any:
-        print(f"Seen {obj} object with id {id(obj)}")
-        self.seen[id(obj)] = None
         if self.is_pickleable(obj):
-            print(f"{obj} is fine.")
             self.seen[id(obj)] = obj
             return obj
         else:
@@ -116,7 +105,6 @@ class UnpickleableItemHelper:
     def handle_unpickleable(self, obj):
         if self.is_endpoint(obj):
             ret = LostObject(obj)
-            print(f"{obj} is an endpoint and will be lost.")
             self.seen[id(obj)] = ret
             return ret
         else:
@@ -127,20 +115,17 @@ class UnpickleableItemHelper:
             shallow_copy = copy.copy(obj)
         except TypeError:
             ret = LostObject(obj)
-            print(f"{obj} could not be copied and will be lost.")
             self.seen[id(obj)] = ret
             return ret
         else:
+            self.seen[id(obj)] = shallow_copy
             return self.handle_shallow_copy(shallow_copy)
 
     def handle_shallow_copy(self, obj):
         if not hasattr(obj, "__dict__"):
-            ret = self.handle_iterable(obj)
+            return self.handle_iterable(obj)
         else:
-            ret = self.handle_namespace_item(obj)
-
-        self.seen[id(obj)] = ret
-        return ret
+            return self.handle_namespace_item(obj)
 
     def handle_iterable(self, obj):
         for index, val in enumerate(obj):
@@ -161,41 +146,9 @@ class UnpickleableItemHelper:
         except pickle.PicklingError:
             return LostObject(obj)
         except TypeError:
-            if id(obj) in self.seen:
-                if self.seen[id(obj)] is not None:
-                    return self.seen[id(obj)]
-                else:
-                    return FutureObject(id(obj))
-            else:
-                return self.recursively_strip_invalid(obj)
+            return self.seen[id(obj)] if id(obj) in self.seen else self.recursively_strip_invalid(obj)
         else:
             return obj
-
-    def fill_in_future_objects(self, obj) -> Any:
-        self.seen_while_filling.add(id(obj))
-        if not self.is_endpoint(obj):
-            if hasattr(obj, "__dict__"):
-                self.fill_in_attributes(obj)
-            else:
-                self.fill_in_members(obj)
-
-    def fill_in_attributes(self, obj) -> Any:
-        for key, val in vars(obj).items():
-            if isinstance(val, FutureObject):
-                setattr(obj, key, self.seen[val.id])
-
-        for val in vars(obj).values():
-            if id(val) not in self.seen_while_filling:
-                self.fill_in_future_objects(val)
-
-    def fill_in_members(self, obj) -> Any:
-        for index, val in enumerate(obj):
-            if isinstance(val, FutureObject):
-                obj[index] = self.seen[val.id]
-
-        for val in obj:
-            if id(val) not in self.seen_while_filling:
-                self.fill_in_future_objects(val)
 
     @staticmethod
     def is_pickleable(item: Any) -> bool:
