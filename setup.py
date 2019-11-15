204c9cf880856a2ccfb862bb762986adc2b682ea
matthewgdv <matthewgdv@gmail.com>
2019-11-15 12:47:42 +0000

    Refactor setup.py to read requirements.txt

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    dependencies = [line for line in f if line]
+
 setup(
     name="pymiscutils",
     version=__version__,
@@ -22,18 +25,7 @@ setup(
       "Programming Language :: Python :: 3.7",
     ],
     packages=find_packages(exclude=["tests*"]),
-    install_requires=[
-        "colorama",
-        "cryptography",
-        "cursor",
-        "dill",
-        "django",
-        "maybe-else",
-        "pathmagic",
-        "readchar",
-        "pysubtypes",
-        "pyinstrument"
-    ],
+    install_requires=dependencies,
     author="Matt GdV",
     author_email="matthewgdv@gmail.com"
 )
