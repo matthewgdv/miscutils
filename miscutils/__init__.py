c747cd574a490cab19a80029697ffbc9cf5825aa
matthewgdv <matthewgdv@gmail.com>
2020-02-13 13:44:39 +0000

    Refactor PrintLog and SqlLog functionality for StoredProcedure logging implementation

--- miscutils/__init__.py
+++ miscutils/__init__.py
@@ -3,11 +3,11 @@ __all__ = [
     "cached_property", "Version", "Counter", "PercentagePrinter", "WindowsEnVars", "WhoCalledMe", "OneOrMany", "Base64", "Gender",
     "Timer", "Supressor", "FilePrintRedirector", "StreamPrintRedirector", "NullContext", "Profiler", "Printer",
     "NestedParser",
-    "ReprMixin", "CopyMixin",
+    "ReprMixin", "CopyMixin", "StreamReplacerMixin",
 ]
 
 from .functions import is_running_in_ipython, executed_within_user_tree, issubclass_safe, is_non_string_iterable, class_name, traceback_from_exception, beep, get_short_lambda_source
 from .base import cached_property, Version, Counter, PercentagePrinter, WindowsEnVars, WhoCalledMe, OneOrMany, Base64, Gender
 from .context import Timer, Supressor, FilePrintRedirector, StreamPrintRedirector, NullContext, Profiler, Printer
 from .parser import NestedParser
-from .mixin import ReprMixin, CopyMixin
+from .mixin import ReprMixin, CopyMixin, StreamReplacerMixin
