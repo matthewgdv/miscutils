b830bfce255bf18749eaba803397371be4ae62c7
matthewgdv <matthewgdv@gmail.com>
2021-03-10 23:25:17 +0000

    Add ParametrizableMixin

--- miscutils/mixin.py
+++ miscutils/mixin.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
 
 from copy import copy, deepcopy
+from typing import Any, Generic, TypeVar
+
+
+T = TypeVar("T")
 
 
 class ReprMixin:
@@ -14,3 +18,25 @@ class CopyMixin:
 
     def deepcopy(self) -> CopyMixin:
         return deepcopy(self)
+
+
+class ParametrizableMixin:
+    class ParametrizedProxy(Generic[T]):
+        def __init__(self, cls: T, param: Any) -> None:
+            self.cls, self.param = cls, param
+
+        def __repr__(self) -> str:
+            return f"{repr(self.cls)}[{repr(self.params)}]"
+
+        def __call__(self, *args, **kwargs) -> T:
+            return self.cls(*args, **kwargs).parametrize(self.param)
+
+    def __class_getitem__(cls, item: Any) -> ParametrizableMixin.ParametrizedProxy:
+        return cls.ParametrizedProxy(cls=cls, param=item)
+
+    def __getitem__(self, param) -> ParametrizableMixin:
+        self.parametrize(param)
+        return self
+
+    def parametrize(self, param: Any) -> ParametrizableMixin:
+        raise NotImplementedError
